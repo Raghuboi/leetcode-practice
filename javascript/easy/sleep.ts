d49e0dc8571bf16f77580b31bc0ec5d194912b4d
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2023-07-16 07:33:43 +0530

    Create sleep.ts
    Q 2621 on leetcode

---
+++ javascript/easy/sleep.ts
@@ -0,0 +1,9 @@
+// Q) Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+async function sleep(millis: number): Promise<void> {
+	await new Promise((resolve) => setTimeout(resolve, millis))
+	// when the setTimeout execution finishes, resolve() is called
+}
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
