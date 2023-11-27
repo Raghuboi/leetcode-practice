c5b2107f7cefd1c61d894d71e977b93d33c94c98
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2023-11-27 10:56:29 -0600

    Create one_fn_call.ts

---
+++ javascript/easy/one_fn_call.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/allow-one-function-call/
+type JSONValue =
+	| null
+	| boolean
+	| number
+	| string
+	| JSONValue[]
+	| { [key: string]: JSONValue }
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+	let called = false
+	return (...args) => {
+		if (called) return undefined
+		called = true
+		return fn(...args)
+	}
+}
+
+let fn = (a: number, b: number, c: number) => a + b + c
+let onceFn = once(fn)
+
+console.log(onceFn(1, 2, 3)) // 6
+console.log(onceFn(2, 3, 6)) // returns undefined without calling fn
